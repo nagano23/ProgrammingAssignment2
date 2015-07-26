361e5bf4d64f45a2feb968684d2d608c0c391373
maos <cnagano@gmail.com>
2015-07-26 11:14:12 -0700

    Updated stubs with matrix inversion code
    Fleshed out matrix inversion functions per example code; added comments
    with test matrix sample

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+##
+## Helper functions for inverting matrices
+## cache results for re-use
+##
 
-## Write a short comment describing this function
+## makeCacheMatrix()
+##
+## Take invertible matrix as parameter,setup
+## list of functions for use in cacheSolve()
+##
+## Quick 3x3 test matrix:
+## tm <- 
+##  matrix(sample.int(15, 9*100, TRUE), 3, 3)
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse) 
+  
 }
 
-
-## Write a short comment describing this function
+## Take result of makeCacheMatrix(), return
+## inverse. Use existing "cached" result if
+## value exists for "m"
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+  
 }
